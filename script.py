3b55ade08521938bb0e49a1c64b030ed52015fd2
Priit Adler <adler.priit@gmail.com>
2024-05-09 15:21:06 +0300

    modified script in main branch as well to create the conflict

--- script.py
+++ script.py
@@ -1,1 +1,13 @@
+import sys
+import os
+
 print("hello world")
+
+print("What is the sum of 2 + 2?")
+# read the answer from the user
+answer = input()
+# print the answer
+print("You answered: " + answer)
+# test if the answer is correct
+if answer == "4":
+    print("Correct!")
